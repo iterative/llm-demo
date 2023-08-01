8c6fabbaebcfa714989d64b15562b072317800a5
Ryan Turner <ryanturner@Ryans-MBP.lan>
2023-08-01 10:29:10 -0700

    adding progress bar to open ai calls

--- vector_store.py
+++ vector_store.py
@@ -4,15 +4,19 @@ from langchain.embeddings import OpenAIEmbeddings
 import pickle
 import json
 
-
 with open("docs.json", "r") as f:
     docs = json.load(f)
 
 with open("metadatas.json", "r") as f:
     metadatas = json.load(f)
 
+print(f"Processing {len(docs)} documents.")
+
+emb = OpenAIEmbeddings()
+emb.show_progress_bar = True
+
 # Here we create a vector store from the documents and save it to disk.
-store = FAISS.from_texts(docs, OpenAIEmbeddings(), metadatas=metadatas)
+store = FAISS.from_texts(docs, emb, metadatas=metadatas)
 faiss.write_index(store.index, "docs.index")
 store.index = None
 with open("faiss_store.pkl", "wb") as f:
