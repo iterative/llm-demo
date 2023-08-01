77ff3af07580cce7e457d975ed170f0472b28cb4
Ryan Turner <ryanturner@Ryans-MBP.lan>
2023-08-01 14:16:59 -0700

    making final qa a stage

--- qa.py
+++ qa.py
@@ -3,11 +3,7 @@ import faiss
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQAWithSourcesChain
 import pickle
-import argparse
-
-parser = argparse.ArgumentParser(description='Ask a question to the notion DB.')
-parser.add_argument('question', type=str, help='The question to ask the notion DB')
-args = parser.parse_args()
+import pandas as pd
 
 # Load the LangChain.
 index = faiss.read_index("docs.index")
@@ -15,8 +11,23 @@ index = faiss.read_index("docs.index")
 with open("faiss_store.pkl", "rb") as f:
     store = pickle.load(f)
 
+with open("samples.txt", "r") as f:
+    sample_questions = f.readlines()
+
 store.index = index
 chain = RetrievalQAWithSourcesChain.from_chain_type(llm=ChatOpenAI(temperature=0), retriever=store.as_retriever())
-result = chain({"question": args.question})
-print(f"Answer: {result['answer']}")
-print(f"Sources: {result['sources']}")
+
+records = []
+for question in sample_questions:
+    question = question.strip()
+    print(f"Question: {question}")
+
+    result = chain({"question": question})
+    records.append({"Q": question, "A": {result["answer"].strip()}, "sources": result['sources'].strip()})
+
+    print(f"Answer: {result['answer']}")
+    print(f"Sources: {result['sources']}")
+    print("")
+
+df = pd.DataFrame.from_records(records)
+df.to_csv("results.csv", header=True, index=False)
