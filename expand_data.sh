97a1ab17a6079b304c359de50cf73f6f0cbbdd79
Ryan Turner <ryanturner@Ryans-MBP.lan>
2023-08-07 14:58:27 -0700

    fixing up dir check

--- expand_data.sh
+++ expand_data.sh
@@ -1,2 +1,2 @@
 # Also makes sure directory is currently empty
-[ -d "data" ] && ditto -V -x -k --sequesterRsrc --rsrc docs_dump.zip data
+[ ! -d "data" ] && ditto -V -x -k --sequesterRsrc --rsrc docs_dump.zip data
